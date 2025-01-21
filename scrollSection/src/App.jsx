7e9217d2d0b15426018fb22db31cf65f482e591f
Ifra Shamim <ifrashamim29@gmail.com>
2025-01-21 09:09:37 +0500

    Done!

--- scrollSection/src/App.jsx
+++ scrollSection/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useRef } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const section1Ref = useRef(null);
+  const section2Ref = useRef(null);
+  const section3Ref = useRef(null);
+  const scrollToSection = (sectionRef) => {
+    sectionRef.current.scrollIntoView({ behavior: 'smooth' });
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <nav>
+        <button onClick={() => scrollToSection(section1Ref)}>Go to Section 1</button>
+        <button onClick={() => scrollToSection(section2Ref)}>Go to Section 2</button>
+        <button onClick={() => scrollToSection(section3Ref)}>Go to Section 3</button>
+      </nav>
+     <div ref={section1Ref} style={{ height: '100vh', width: '90vw' , backgroundColor: '#f4f4f4' }}>
+        <h2>Section 1</h2>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div ref={section2Ref} style={{ height: '100vh', backgroundColor: '#c4c4c4' }}>
+        <h2>Section 2</h2>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div ref={section3Ref} style={{ height: '100vh', backgroundColor: '#a4a4a4' }}>
+        <h2>Section 3</h2>
+      </div>
     </>
   )
 }
