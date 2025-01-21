dfc32e592759437829d1022f08d8d4f68b8af01e
Ifra Shamim <ifrashamim29@gmail.com>
2025-01-21 09:58:26 +0500

    Done!

--- videoPlayer/src/App.jsx
+++ videoPlayer/src/App.jsx
@@ -1,14 +1,34 @@
-import { useState } from 'react'
-import './App.css'
+import { useRef } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const videoRef = useRef(null);
+
+  const playVideo = () => {
+    if (videoRef.current) {
+      videoRef.current.play();
+    }
+  };
+
+  const pauseVideo = () => {
+    if (videoRef.current) {
+      videoRef.current.pause();
+    }
+  };
 
   return (
-    <>
-      
-    </>
-  )
+    <div>
+      <h2>Custom Video Player</h2>
+      <video ref={videoRef} width="600" controls>
+        <source src="path-to-your-video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div>
+        <button onClick={playVideo}>Play</button>
+        <button onClick={pauseVideo}>Pause</button>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
