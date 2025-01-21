bb214ba5ac2fc369e36fc60f8a5775e4a22876e1
Ifra Shamim <ifrashamim29@gmail.com>
2025-01-21 08:58:23 +0500

    Done!

--- countdownTimer/src/App.jsx
+++ countdownTimer/src/App.jsx
@@ -1,34 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState , useRef } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [time, setTime] = useState(60)
+  const [isRunning, setIsRunning] = useState(false);
+  const timerRef = useRef(null);
+  const startTimer = () => {
+    if(!isRunning){
+      setIsRunning(true);
+      timerRef.current = setInterval(()=>{
+        setTime((prevTime)=> {
+          if(prevTime <= 1){
+            clearInterval(timerRef.current)
+            setIsRunning(false)
+            return 0;
+          }
+          return prevTime - 1;
+        })
+      },1000)
+    }
+  }
+  const pauseTimer = () => {
+    if(isRunning){
+      clearInterval(timerRef.current);
+      setIsRunning(false)
+    }
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <h1>Countdown Timer</h1>
+     <p>Time Remaining: {time} seconds.</p>
+     <button onClick={startTimer} disabled={isRunning}>{
+      isRunning ? 'Running...' : 'Start/Resume'}</button><br /><br />
+      <button onClick={pauseTimer} disabled={!isRunning}>Pause</button>
     </>
+
   )
 }
 
